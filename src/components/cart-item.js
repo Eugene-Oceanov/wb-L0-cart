0dabcb29b27ca50ecfb61a4d10de2c6a3e9797cd
Eugene Oceanov <kenoby1994@gmail.com>
2023-10-12 16:23:41 +0300

    technical commit

--- src/components/cart-item.js
+++ src/components/cart-item.js
@@ -1,19 +1,35 @@
 module.exports = {
-    fullCartItem: function (item) {
+    CartItemAviable: function (item) {
         let cartItem = document.createElement("DIV");
-        cartItem.classList.add("cart-item????");
+        cartItem.classList.add("cart-item", "d-flex");
         cartItem.innerHTML = `
-            <input type="checkbox" id="cart-item-${item.id}">
-
-            <label for="cart-item-${item.id}" class="d-flex jc-space-between">
-                <div class="cart-item-info">
-                    <img src="${item.img}" alt="${item.title}" class="cart-item-info__img">
-                    <div class="cart-item-info__text">
-                        <h3>${item.title}</h3>
-
+            <input type="checkbox" id="cart-item-${item.id}__cb">
+            <label for="cart-item-${item.id}__cb" class="cart-item__checkbox d-flex jc-space-between"></label>
+            <div class="cart-item-wrapper d-flex">
+                <a href="#"><img src="${item.img}" alt="${item.title}" class="cart-item-info__img"></a>
+                <div class="cart-item-info d-flex jc-space-between">
+                    <div class="cart-item-info__description">
+                        <h2 class="cart-item__title">${item.title}</h2>
+                        <div class="cart-item-properties d-flex">
+                            ${item.color ? `<p class="cart-item-properties__item">Цвет: ${item.color}</p>` : ""}
+                            ${item.size ? `<p class="cart-item-properties__item">Размер: ${item.size}</p>` : ""}
+                        </div>
+                        <p class="cart-item__stock">${item.stock}</p>
+                        <p class="cart-item__manufacturer">${item.manufacturer}</p>
+                    </div>
+                    <div class="cart-item-options">
+                        <div class="cart-item-options-panel">
+                            <div class="cart-item-options-panel-quantity d-flex jc-space-between ai-center">
+                                <p class="cart-item-options-panel__quantity-lower">-</p>
+                                <input type="text" class="cart-item-options-panel__quantity-input">
+                                <p class="cart-item-options-panel__quantity-increase">+</p>
+                            </div>
+                            ${item.remainder <= 3 ? `<p class="">Осталось ${item.remainder} шт.</p>` : ""}
+                        </div>
                     </div>
                 </div>
-            </label>
+            </div>
+            
         `
         return cartItem;
     }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 
 import "./assets/css/style.css";
 const cartItem = require("./components/cart-item.js");
-const basket = document.querySelector(".basket");
+const cartAviable = document.querySelector(".cart-aviable__container");
+
 
 async function getCartData() {
     let json = await fetch("https://raw.githubusercontent.com/Eugene-Oceanov/wb-L0-cart/main/src/json/cart-api.json");
     let data = await json.json();
-    console.log(data)
     data.forEach(item => {
-        basket.append(cartItem.fullCartItem(item));
+        if (item.remainder > 0) cartAviable.append(cartItem.CartItemAviable(item));
     })
 }
 
